ee7de4a8249ac7d7a3b135360e7fd6accb6f5ce4
agauchat <augusto.gauchat@mercadolibre.com>
2020-11-02 02:06:58 -0300

    Implements pallete for images

--- app/src/main/java/com/gaucho/android/tvshowreminder/DetailActivity.kt
+++ app/src/main/java/com/gaucho/android/tvshowreminder/DetailActivity.kt
@@ -2,12 +2,20 @@ package com.gaucho.android.tvshowreminder
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import com.gaucho.android.tvshowreminder.model.TVSerie
 import com.gaucho.android.tvshowreminder.utils.setupTextView
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Picasso.LoadedFrom
 import kotlinx.android.synthetic.main.activity_detail.*
 import java.util.*
+
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,7 +27,7 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun show(serie: TVSerie) {
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.hide()
         detail_name.setupTextView(serie.name)
         detail_overview.setupTextView(serie.overview)
         var year : String? = null
@@ -29,6 +37,25 @@ class DetailActivity : AppCompatActivity() {
             year = cal.get(Calendar.YEAR).toString()
         }
         detail_release_year.setupTextView(year)
+        Picasso.get().load("https://image.tmdb.org/t/p/w780${serie.backdropPath}").into(detail_background_image)
+        detail_background_image.colorFilter = ColorMatrixColorFilter(ColorMatrix().apply { setSaturation(0f)})
+        detail_background_image.alpha = 0.5F
+
+        Picasso.get().load("https://image.tmdb.org/t/p/w300${serie.posterPath}").into(object : com.squareup.picasso.Target {
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: LoadedFrom?) {
+                bitmap?.let {
+                    val palette: Palette = Palette.from(bitmap).generate()
+                    val dominantColor = palette.getDominantColor(getColor(R.color.card_gradient_start))
+                    background_transparency.setBackgroundColor(dominantColor)
+                    background_transparency.alpha = 0.7F
+                };
+            }
+
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
+            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {}
+            })
+
+        Picasso.get().load("https://image.tmdb.org/t/p/w300${serie.posterPath}").into(detail_top_image)
     }
 
     companion object {
